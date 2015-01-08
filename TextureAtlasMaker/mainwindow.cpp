7b4de78ea75b8ec2899a6a5542894207b07b00f5
Tobias <iso8859-1@gmx.de>
2015-01-08 19:13:19 +0100

    added required widgets to layout

--- TextureAtlasMaker/mainwindow.cpp
+++ TextureAtlasMaker/mainwindow.cpp
@@ -3,8 +3,9 @@
 #include <QPushButton>
 #include <QComboBox>
 #include <QLabel>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QCheckBox>
+#include <QListView>
 
 MainWindow::MainWindow(QWidget* parent)
 : QMainWindow(parent)
@@ -17,21 +18,35 @@ MainWindow::MainWindow(QWidget* parent)
 TextureAtlasMakerWidget::TextureAtlasMakerWidget(QWidget* parent)
 : QWidget(parent)
 {
+    auto centralLayout = new QGridLayout(this);
+
     auto labelTextureSize = new QLabel("Texture Size:",this);
     auto comboBoxTextureSize = new QComboBox(this);
-    auto layoutTextureSize = new QHBoxLayout();
-    layoutTextureSize->addWidget(labelTextureSize);
-    layoutTextureSize->addWidget(comboBoxTextureSize);
+    centralLayout->addWidget(labelTextureSize, 0, 0);
+    centralLayout->addWidget(comboBoxTextureSize, 0, 1);
+    centralLayout->setColumnStretch(1, 1);
+    centralLayout->setRowStretch(100, 1);
     
     auto labelTileSize = new QLabel("Tile Size:", this);
     auto comboBoxTileSize = new QComboBox(this);
-    auto layoutTileSize = new QHBoxLayout();
-    layoutTileSize->addWidget(labelTileSize);
-    layoutTileSize->addWidget(comboBoxTileSize);
+    centralLayout->addWidget(labelTileSize, 1, 0);
+    centralLayout->addWidget(comboBoxTileSize, 1, 1);
     
-    auto centralLayout = new QVBoxLayout(this);
-    centralLayout->addLayout(layoutTextureSize);
-    centralLayout->addLayout(layoutTileSize);
+    auto lableTileResize = new QLabel("Resize Tile Graphics?",this);
+    auto checkboxTileResize = new QCheckBox(this);
+    centralLayout->addWidget(lableTileResize,2,0);
+    centralLayout->addWidget(checkboxTileResize,2,1);
+    
+    auto listOfFiles = new QListView(this);
+    centralLayout->addWidget(listOfFiles,3,0,1,-1);
+    
+    auto addButton = new QPushButton("Add",this);
+    centralLayout->addWidget(addButton,4,0);
+    auto removeButton = new QPushButton("Remove",this);
+    centralLayout->addWidget(removeButton,4,1);
+    
+    auto generateButton = new QPushButton("Generate",this);
+    centralLayout->addWidget(generateButton,5,0,1,-1);
     
     setLayout(centralLayout);
 }
