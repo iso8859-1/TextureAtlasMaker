abb3c10706e556f0535797944baca551174bde20
Tobias <iso8859-1@gmx.de>
2015-01-10 22:44:15 +0100

    small refactoring to reduce code duplication

--- TextureAtlasMaker/mainwindow.cpp
+++ TextureAtlasMaker/mainwindow.cpp
@@ -98,32 +98,25 @@ TextureAtlasMakerWidget::TextureAtlasMakerWidget(QWidget* parent)
     
     listOfFiles->setModel(_list);
     
+    auto adaptLabel = [=](int index){
+        //sizes as power of 2
+        int textureSize = comboBoxTextureSize->currentIndex()+1;
+        int tileSize = comboBoxTileSize->currentIndex()+1;
+        //divide by subtracting the exponent and multiply by itself by squaring it
+        int numberOfTilesExponent = (textureSize - tileSize)+(textureSize - tileSize);
+        int numberOfTiles = 1 << numberOfTilesExponent;
+        int numberOfListEntries = _list->rowCount();
+        statusLabel->setText(QString::number(numberOfListEntries)+" of "+QString::number(numberOfTiles)+" slots taken");
+    };
+    
     
     connect(comboBoxTextureSize
             ,indexChanged
-            ,[=](int index){
-                //sizes as power of 2
-                int textureSize = comboBoxTextureSize->currentIndex()+1;
-                int tileSize = comboBoxTileSize->currentIndex()+1;
-                //divide by subtracting the exponent and multiply by itself by squaring it
-                int numberOfTilesExponent = (textureSize - tileSize)+(textureSize - tileSize);
-                int numberOfTiles = 1 << numberOfTilesExponent;
-                int numberOfListEntries = _list->rowCount();
-                statusLabel->setText(QString::number(numberOfListEntries)+" of "+QString::number(numberOfTiles)+" slots taken");
-            });
+            ,adaptLabel);
     
     connect(comboBoxTileSize
             ,indexChanged
-            ,[=](int index){
-                //sizes as power of 2
-                int textureSize = comboBoxTextureSize->currentIndex()+1;
-                int tileSize = comboBoxTileSize->currentIndex()+1;
-                //divide by subtracting the exponent and multiply by itself by squaring it
-                int numberOfTilesExponent = (textureSize - tileSize)+(textureSize - tileSize);
-                int numberOfTiles = 1 << numberOfTilesExponent;
-                int numberOfListEntries = _list->rowCount();
-                statusLabel->setText(QString::number(numberOfListEntries)+" of "+QString::number(numberOfTiles)+" slots taken");
-            });
+            ,adaptLabel);
     
     auto addButton = new QPushButton("Add",this);
     auto buttonlayout = new QHBoxLayout();
